07cb72f00bdaed4eeb97c4ba9a3d4e6a81138bf6
Ahmed <ahm3d.ad3l@gmail.com>
2018-11-14 20:37:40 +0200

    adding extensions, working on fixing multiple login clicks

--- app/src/main/java/com/mvi/presentation/Extensions.kt
+++ app/src/main/java/com/mvi/presentation/Extensions.kt
@@ -1,5 +1,15 @@
 package com.mvi.presentation
 
+import android.content.Context
 import android.widget.TextView
+import android.widget.Toast
 
 fun TextView.asString() = text?.toString() ?: ""
+
+fun Context.longToast(message: String) {
+    Toast.makeText(this,message,Toast.LENGTH_LONG).show()
+}
+
+fun Context.shortToast(message: String) {
+    Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+}
